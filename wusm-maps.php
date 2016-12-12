03ec22d393ba73e5cf2437719f68b0ab9285c6b8
Aaron Graham <aegraham@gmail.com>
2016-12-12 16:06:32 -0600

    Can't use SVG for custom map markers :()

--- wusm-maps.php
+++ wusm-maps.php
@@ -356,7 +356,7 @@ class wusm_maps_plugin {
  				
  				$map_url = "https://maps.googleapis.com/maps/api/staticmap?";
 				$map_options = "center=$lat,$lng&zoom=15&size=300x220&markers=color:red%7C$lat,$lng";
-				$map_icon = "&icon=" . WUSM_MAPS_PLUGIN_URL . "location.svg";
+				$map_icon = "&icon=" . WUSM_MAPS_PLUGIN_URL . "location.png";
 				$map_styling = "&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0xf5f5f5&style=element:labels.icon%7Cvisibility:off&style=element:labels.text.fill%7Ccolor:0x616161&style=element:labels.text.stroke%7Ccolor:0xf5f5f5&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:poi%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:road%7Celement:geometry%7Ccolor:0xffffff&style=feature:road.arterial%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:road.highway%7Celement:geometry%7Ccolor:0xdadada&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:transit.line%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:transit.station%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:water%7Celement:geometry%7Ccolor:0xc9c9c9&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x9e9e9e";
 
  				echo "<div class='wusm-maps-section'>";
